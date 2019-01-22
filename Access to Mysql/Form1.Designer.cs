155eef84360f665f4e518a8f8cb5638738f21aec
patelshyam <patelshyam54@gmail.com>
2019-01-22 13:03:51 +0530

    final_upload

---
+++ Access to Mysql/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Access_to_Mysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.access_file = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.db = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.host = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.access_pass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // access_file
+            // 
+            this.access_file.Enabled = false;
+            this.access_file.Location = new System.Drawing.Point(91, 149);
+            this.access_file.Name = "access_file";
+            this.access_file.Size = new System.Drawing.Size(274, 20);
+            this.access_file.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Access File :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(371, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.db);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.username);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.host);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(26, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 161);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mysql";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 112);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Database : ";
+            // 
+            // db
+            // 
+            this.db.Location = new System.Drawing.Point(85, 109);
+            this.db.Name = "db";
+            this.db.Size = new System.Drawing.Size(194, 20);
+            this.db.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(80, 109);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(23, 86);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(59, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Password :";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(85, 83);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(194, 20);
+            this.password.TabIndex = 15;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(80, 83);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "UserName:";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(85, 57);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(194, 20);
+            this.username.TabIndex = 12;
+            this.username.Text = "root";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(80, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Host :";
+            // 
+            // host
+            // 
+            this.host.Location = new System.Drawing.Point(84, 31);
+            this.host.Name = "host";
+            this.host.Size = new System.Drawing.Size(194, 20);
+            this.host.TabIndex = 9;
+            this.host.Text = "127.0.0.1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // access_pass
+            // 
+            this.access_pass.Location = new System.Drawing.Point(91, 175);
+            this.access_pass.Name = "access_pass";
+            this.access_pass.Size = new System.Drawing.Size(165, 20);
+            this.access_pass.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "( if applicable otherwise remain it blank)";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 427);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(420, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(467, 115);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(468, 480);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.access_pass);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.access_file);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Access To Mysql";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox access_file;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox host;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox access_pass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox db;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Access to Mysql/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Access_to_Mysql
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn = new OleDbConnection();
+        string pass = "OPENIT";
+        List<string> tableName = new List<string>();
+        MySqlConnection connection;
+        OpenFileDialog ofd = new OpenFileDialog();
+        string access = null;
+        string host_name = null;
+        string user_name = null;
+        string pass_of_mysql = null;
+        string db_name = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            host_name = host.Text;
+            user_name = username.Text;
+            pass_of_mysql = password.Text;
+            db_name = db.Text;
+            pass = access_pass.Text;
+
+
+            if (!(String.IsNullOrEmpty(access) || String.IsNullOrEmpty(host_name) || String.IsNullOrEmpty(user_name) || String.IsNullOrEmpty(db_name)))
+            {
+                conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+                @"Data source=" + access + ";"
+                + "Jet OLEDB:Database Password=" + pass + ";";
+
+                open_conn_access();
+                get_all_table();
+            }
+            else
+            {
+                MessageBox.Show("Please Select All data First");
+            }
+        }
+
+        public void open_conn_access()
+        {
+
+            try
+            {
+                conn.Open();
+                //MessageBox.Show("Connected success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Access Connection is failed. Check if there is password or not");
+            }
+
+        } // Access connection
+
+        public void close_conn_access()
+        {
+            conn.Close();
+        } //Access connection close
+
+        public void open_conn_mysql()
+        {
+            MySqlConnectionStringBuilder conn_str = new MySqlConnectionStringBuilder();
+            conn_str.Server = host_name;
+            conn_str.UserID = user_name;
+            conn_str.Password = pass_of_mysql;
+            conn_str.Database = db_name;
+            connection = new MySqlConnection(conn_str.GetConnectionString(true));
+            try
+            {
+                connection.Open();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Mysql Database Connection Failed");
+            }
+        } //open mysql connection 
+
+        public void get_all_table()
+        {
+            DataTable dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables,new object[] {null,null,null, "TABLE" });
+            open_conn_mysql();
+            List<string> allrows = new List<string>();
+            List<string> list_table = new List<string>();
+            int progress = 0;
+            int number_of_tables = dt.Rows.Count;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = number_of_tables;
+            try
+            {
+                foreach (DataRow tr in dt.Rows)
+                {
+                    progressBar1.Value = progress;
+                    string tbName = tr[2].ToString();
+                    Console.WriteLine("*********************  " + tbName + "  ******************");
+                    if (!tbName.Contains("MSys"))
+                    {
+                        string[] rest = new string[] { null, null, tbName, null };
+                        DataTable rows = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Columns, rest);
+                        List<string> datatype_list = new List<string>();
+                        allrows.Clear();
+                        foreach (DataRow rr in rows.Rows)
+                        {
+                            string tbrow = rr["COLUMN_NAME"].ToString();
+                            int colnum_type = Int32.Parse(rr["DATA_TYPE"].ToString());
+                            string datatype = get_dataType(colnum_type);
+                            datatype_list.Add(datatype);
+                            if (!(Boolean)rr["IS_NULLABLE"])
+                            {
+                                allrows.Add(tbrow + " " + datatype);
+                            }
+                            else
+                            {
+                                allrows.Add(tbrow + " " + datatype + " NULL");
+                            }
+                        }
+                        string columns = String.Join(",", allrows);
+                        int sizeofrow = allrows.Count;
+                        string query = "CREATE TABLE " + tbName + "(" + columns + ")";
+                        MySqlCommand cmd = new MySqlCommand(query, connection);
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine(query);
+                        add_primary_key(tbName);
+                        string[] temp = datatype_list.ToArray();
+                        transfar_data(tbName, sizeofrow, temp);
+                        list_table.Add(tbName);
+                    }
+                    progress++;
+                }
+            }catch(Exception e)
+            {
+
+            }
+            add_forign_key();
+            progressBar1.Value = number_of_tables;
+            MessageBox.Show("Ok!!");
+        } //close mysql connection
+
+
+
+        public void add_primary_key(string tbName)
+        {
+            DataTable primarykeyTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Primary_Keys, new Object[] { null, null, tbName });
+            int columnOrdinalForname = primarykeyTable.Columns["COLUMN_NAME"].Ordinal;
+            string query_pk = "ALTER TABLE " + tbName + " ADD PRIMARY KEY(";
+            int count = primarykeyTable.Rows.Count;
+            int i = 0;
+            foreach (DataRow r in primarykeyTable.Rows)
+            {
+                string pk = r.ItemArray[columnOrdinalForname].ToString();
+                if (count > i + 1)
+                {
+                    query_pk = String.Concat(query_pk, pk + ",");
+                }
+                else
+                {
+                    query_pk = String.Concat(query_pk, pk);
+                }
+                i++;
+            }
+
+            query_pk = query_pk + ")";
+            if (i > 0)
+            {
+                Console.WriteLine(query_pk);
+                MySqlCommand cmd_pk = new MySqlCommand(query_pk, connection);
+                cmd_pk.ExecuteNonQuery();
+            }
+        }// for adding primary keys
+
+        public void transfar_data(string tbName,int size,string[] type)
+        {
+            OleDbCommand access_cmd = new OleDbCommand("SELECT * FROM " + tbName,conn);
+            OleDbDataReader access_reader = access_cmd.ExecuteReader();
+            List<string> data_holder = new List<string>();
+            List<string> data_holder_rows = new List<string>();
+            List<string> data_type = new List<string>();
+            int j = 0;
+            while(access_reader.Read())
+            {
+                data_holder.Clear();
+             for(int i=0; i<size; i++)
+                {
+                    if(j==0)
+                    {
+                        data_type.Add(access_reader.GetName(i));
+                    }
+
+                    if(access_reader.GetDataTypeName(i).Equals("DBTYPE_DATE"))
+                    {
+                        string[] temp_date = access_reader.GetValue(i).ToString().Split(' ');
+                        if (!String.IsNullOrEmpty(temp_date[0]))
+                        {
+                            string[] part_date = temp_date[0].Split('/');
+                            string month = get_month(part_date[1]);
+                            data_holder.Add("'" + part_date[2] + "-" + month + "-" + part_date[0] + "'");
+                        }
+                        else
+                        {
+                            data_holder.Add("'" + "" + "'");
+                        }
+                    }
+                    else
+                    {
+                        string s = access_reader.GetValue(i).ToString();
+                        if(!String.IsNullOrEmpty(s))
+                        {
+                            data_holder.Add("'"+s.ToString().Replace("'","")+"'");
+                        }
+                        else
+                        {
+                            data_holder.Add("NULL");
+                        }
+                    }
+                }
+                string temp = "(" + String.Join(",", data_holder) + ")";
+                data_holder_rows.Add(temp);
+                j++;
+            }
+
+            string query = "INSERT INTO " + tbName + " (" + String.Join(",",data_type) + ") " + " VALUES " + String.Join(",", data_holder_rows);
+            if(data_holder_rows.Count > 0)
+            {
+                MySqlCommand insrtcmd = new MySqlCommand(query,connection);
+                insrtcmd.ExecuteNonQuery();
+            }
+        }//for transfaring the data from access to mysql
+
+        public void add_forign_key()
+        {
+            DataTable forignkeyTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Foreign_Keys, new Object[] { null });
+            string query = null;
+            foreach(DataRow row in forignkeyTable.Rows)
+            {
+                string Name = (string)row["FK_NAME"];
+                string PrimaryTable = (string)row["PK_TABLE_NAME"];
+                string PrimaryField = (string)row["PK_COLUMN_NAME"];
+                string PrimaryIndex = (string)row["PK_NAME"];
+                string ForeignTable = (string)row["FK_TABLE_NAME"];
+                string ForeignField = (string)row["FK_COLUMN_NAME"];
+
+                query = "ALTER TABLE " + ForeignTable + " ADD FOREIGN KEY (" + ForeignField + ") REFERENCES " + PrimaryTable + "(" + PrimaryField + ");";
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.ExecuteNonQuery();
+                }catch(Exception e)
+                {
+                    Console.WriteLine("There is some problem with database please check");
+                }
+            }
+        }
+
+
+
+        public string get_month(string month)
+        {
+            switch(month)
+            {
+                case "Jan":
+                    return "01";
+                case "Feb":
+                    return "02";
+                case "Mar":
+                    return "03";
+                case "Apr":
+                    return "04";
+                case "May":
+                    return "05";
+                case "Jun":
+                    return "06";
+                case "Jul":
+                    return "07";
+                case "Aug":
+                    return "08";
+                case "Sep":
+                    return "09";
+                case "Oct":
+                    return "10";
+                case "Nov":
+                    return "11";
+                case "Dec":
+                    return "12";
+            }
+            return null;
+        } //convert month form text to number
+
+        public string get_dataType(int i)
+        {
+            switch ((OleDbType)(i))
+            {
+                case OleDbType.WChar:
+                    return "VARCHAR(600)";
+                case OleDbType.Double:
+                    return "DOUBLE";
+                case OleDbType.Date:
+                    return "DATE";
+                case OleDbType.Boolean:
+                    return "VARCHAR(10)";
+                case OleDbType.Integer:
+                    return "INT";
+            }
+            return null;
+        }// for getting datatypes from the specific field
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                access = ofd.FileName;
+                access_file.Text = access;
+            }
+        }
+
+        
+    }
+}
